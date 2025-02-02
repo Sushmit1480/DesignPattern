ca9855c4df7caf854d3e274301c1d9feb42a8089
Sushmit1408 <sushmitpatil1@gmail.com>
2025-02-02 13:13:01 +0530

    added code for design pattern

---
+++ src/com/sushmit/StructuralPattern/FlyweightPattern/BadCode/Game.java
@@ -0,0 +1,5 @@
+package com.sushmit.StructuralPattern.FlyweightPattern.BadCode;
+
+public class Game {
+
+}
---
+++ src/com/sushmit/StructuralPattern/FlyweightPattern/GoodCode/Bullet.java
@@ -0,0 +1,5 @@
+package com.sushmit.StructuralPattern.FlyweightPattern.GoodCode;
+
+public class Bullet {
+
+}
