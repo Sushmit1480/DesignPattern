4540a9555fcac0ad0b9d7c89a5eabcb4517c8416
Sushmit1408 <sushmitpatil1@gmail.com>
2025-03-04 22:43:44 +0530

    added code for design pattern

--- src/com/sushmit/BehaviouralPattern/TemplatePattern/WithoutTemplatePattern.java
+++ src/com/sushmit/BehaviouralPattern/TemplatePattern/WithoutTemplatePattern.java
@@ -1,5 +1,46 @@
 package com.sushmit.BehaviouralPattern.TemplatePattern;
 
+class CSVParser{
+	public void parse() {
+		openFile();
+		// CSV specific Parsing logic
+		System.out.println("Parsing a CSV File");
+		closeFile();
+	}
+	
+	private void openFile() {
+		System.out.println("Opening File");
+	}
+	
+	private void closeFile() {
+		System.out.println("Closing File");
+	}
+}
+
+class JSONParser{
+	public void parse() {
+		openFile();
+		// CSV specific Parsing logic
+		System.out.println("Parsing a JSON File");
+		closeFile();
+	}
+	
+	private void openFile() {
+		System.out.println("Opening File");
+	}
+	
+	private void closeFile() {
+		System.out.println("Closing File");
+	}
+}
+
 public class WithoutTemplatePattern {
 
+	public static void main(String[] args) {
+		CSVParser csvParser = new CSVParser();
+		JSONParser jsonParser = new JSONParser();
+		
+		csvParser.parse();
+		jsonParser.parse();
+	}
 }
