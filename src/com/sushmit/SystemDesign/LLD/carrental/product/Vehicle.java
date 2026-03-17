61f1bd9fde2576c7cd84a0d6472b0b8f48a60b0f
Sushmit1408 <sushmitpatil1@gmail.com>
2026-03-17 17:29:57 +0530

    Code for car rental system design

--- src/com/sushmit/SystemDesign/LLD/carrental/product/Vehicle.java
+++ src/com/sushmit/SystemDesign/LLD/carrental/product/Vehicle.java
@@ -1,49 +1,61 @@
 package com.sushmit.SystemDesign.LLD.carrental.product;
 
 public class Vehicle {
-	
-	private final int vehicleID;
-	private final String vehicleNumber;
-	private final VehicleType vehicleType;
-	private double dailyRentalCost;
-	private volatile VehicleStatus vehicleStatus;
-	
-	public Vehicle(int vehicleID, String vehicleNumber, VehicleType vehicleType) {
-		this.vehicleID = vehicleID;
-		this.vehicleNumber = vehicleNumber;
-		this.vehicleType = vehicleType;
-		this.vehicleStatus = VehicleStatus.AVAILABLE;
-	}
 
-	// ----------------- Getters --------------------------
-	public int getVehicleID() {
-		return vehicleID;
-	}
+    private final int vehicleID;
+    private final String vehicleNumber;
+    private final VehicleType vehicleType;
+    private double dailyRentalCost;
+    private volatile VehicleStatus vehicleStatus;
 
-	public String getVehicleNumber() {
-		return vehicleNumber;
-	}
+    /* we can add more properties like below if required
+        private String companyName;
+        private String modelName;
+        private int kmDriven;
+        private int average;
+        private int cc;
+        private int noOfSeat;
+    */
 
-	public VehicleType getVehicleType() {
-		return vehicleType;
-	}
+    // --------- Constructors ---------
+    public Vehicle(int vehicleID, String vehicleNumber, VehicleType vehicleType) {
+        this.vehicleID = vehicleID;
+        this.vehicleNumber = vehicleNumber;
+        this.vehicleType = vehicleType;
+        this.vehicleStatus = VehicleStatus.AVAILABLE;
+    }
 
-	public double getDailyRentalCost() {
-		return dailyRentalCost;
-	}
 
-	public VehicleStatus getVehicleStatus() {
-		return vehicleStatus;
-	}
+    // --------- Getters ---------
 
-    // ----------------- Setters --------------------------
-	public void setDailyRentalCost(double dailyRentalCost) {
-		this.dailyRentalCost = dailyRentalCost;
-	}
+    public int getVehicleID() {
+        return vehicleID;
+    }
 
-	public void setVehicleStatus(VehicleStatus vehicleStatus) {
-		this.vehicleStatus = vehicleStatus;
-	}
-	
-	
+    public VehicleType getVehicleType() {
+        return vehicleType;
+    }
+
+    public VehicleStatus getVehicleStatus() {
+        return vehicleStatus;
+    }
+
+    public double getDailyRentalCost() {
+        return dailyRentalCost;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    // --------- Setters ---------
+
+    public void setDailyRentalCost(double dailyRentalCost) {
+        this.dailyRentalCost = dailyRentalCost;
+    }
+
+
+    public void setStatus(VehicleStatus vehicleStatus) {
+        this.vehicleStatus = vehicleStatus;
+    }
 }
